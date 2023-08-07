903b03b03f32f7230718aa3024fcd1cbb2f773b2
bouquet1 <bukuleta@gmail.com>
2023-08-07 16:40:06 -0700

    2 passed

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,13 @@
 class Song:
-    pass
+
+    count = 0
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+
+    @classmethod
+    def add_song_to_count(cls, increment=1):
+        cls.count += increment
