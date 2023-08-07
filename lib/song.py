a8a63cfdecddf05b4e64eb5b85f6ae8b466af4c3
bouquet1 <bukuleta@gmail.com>
2023-08-07 16:57:28 -0700

    3 passed

--- lib/song.py
+++ lib/song.py
@@ -1,13 +1,30 @@
 class Song:
 
+    # Create a class attribute, count and set it to 0.
     count = 0
+    # Create a genres attribute, equal to an empty list.
+    genres = []
 
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
+        # Your __init__ method should call a class method that increments the value of count by one.
         self.add_song_to_count()
+        # genres list when a new song is created
+        self.add_to_genres(genre)
+
+    # class method increments the value of count by one
 
     @classmethod
     def add_song_to_count(cls, increment=1):
         cls.count += increment
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        # control for duplicates
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+
+# test ur code
