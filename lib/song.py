df2117908251f645dc929c77b7747e1b648665dd
bouquet1 <bukuleta@gmail.com>
2023-08-07 17:10:38 -0700

    4 passed

--- lib/song.py
+++ lib/song.py
@@ -4,6 +4,8 @@ class Song:
     count = 0
     # Create a genres attribute, equal to an empty list.
     genres = []
+    # artists, that is equal to an empty list.
+    artists = []
 
     def __init__(self, name, artist, genre):
         self.name = name
@@ -13,9 +15,10 @@ class Song:
         self.add_song_to_count()
         # genres list when a new song is created
         self.add_to_genres(genre)
+        # artists list
+        self.add_to_artists(artist)
 
     # class method increments the value of count by one
-
     @classmethod
     def add_song_to_count(cls, increment=1):
         cls.count += increment
@@ -26,5 +29,17 @@ class Song:
         if genre not in cls.genres:
             cls.genres.append(genre)
 
+    @classmethod
+    def add_to_artists(cls, artist):
+        # control for duplicates
+        if artist not in cls.artists:
+            cls.artists.append(artist)
 
-# test ur code
+
+# test ur code python lib/song.py
+# song1 = Song("Song 1", "Artist 1", "Pop")
+# song2 = Song("Song 2", "Artist 2", "Pop")
+# song3 = Song("Song 3", "Artist 3", "Rock")
+
+# print(Song.genres)
+# print(Song.artists)
